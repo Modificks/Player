a181c83ca56466c62f7d9048893efe7b489488a2
Modificks <dimanakonechnui7@gmail.com>
2023-11-06 20:45:17 +0200

    adding basic search in table with JS

---
+++ src/main/resources/templates/JS/MusicSearch.js
@@ -0,0 +1,24 @@
+document.getElementById('searchInput').addEventListener('input', function() {
+
+     let input = document.getElementById('searchInput'),
+         filter = input.value.toUpperCase(),
+         table = document.getElementsByClassName('list-of-songs')[0],
+         tr = table.getElementsByTagName('tr');
+
+    for (let i = 0; i < tr.length; i++) {
+         let td = tr[i].getElementsByTagName('th');
+
+        for (let j = 0; j < td.length; j++) {
+            if (td[j]) {
+                let txtValue = td[j].textContent || td[j].innerText;
+
+                if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                    tr[i].style.display = '';
+                    break;
+                } else {
+                    tr[i].style.display = 'none';
+                }
+            }
+        }
+    }
+});
