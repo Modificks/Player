dc1304df5b31e87a75111eabc931ca3deb3b3b77
Modificks <dimanakonechnui7@gmail.com>
2023-11-07 21:26:34 +0200

    adding mapping to get for player user should log in or register new account

--- src/main/java/com/player/player/controllers/PlayerPageController.java
+++ src/main/java/com/player/player/controllers/PlayerPageController.java
@@ -2,7 +2,9 @@ package com.player.player.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping({"/log-in", "/registration"})
 @Controller
 public class PlayerPageController {
 
